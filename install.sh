0b86aeaffb081c6356c7cf2e81dfeb4faf6624f7
Jamie McDonald <jamie@qwyck.net>
2022-08-10 09:59:57 +0100

    feat: add error to install script if curl and git are not present

--- install.sh
+++ install.sh
@@ -2,18 +2,16 @@
 
 set -e # -e: exit on error
 
+if ! [ -x "$(command -v curl)" ] || ! [ -x "$(command -v git)" ]; then
+    printf "ERROR: To install these dotfiles, you must have curl and git installed.\n" >&2
+    exit 1
+fi
+
 if [ ! "$(command -v chezmoi)" ]; then
-  printf "INFO: Installing chezmoi\n"
+  printf "INFO: installing chezmoi\n"
   bin_dir="$HOME/.local/bin"
   chezmoi="$bin_dir/chezmoi"
-  if [ "$(command -v curl)" ]; then
-    sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$bin_dir"
-  elif [ "$(command -v wget)" ]; then
-    sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b "$bin_dir"
-  else
-    echo "To install chezmoi, you must have curl or wget installed." >&2
-    exit 1
-  fi
+  sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$bin_dir"
 else
   chezmoi=chezmoi
 fi
