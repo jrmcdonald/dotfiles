9b66d8e803ed1484f0336d1ca65347a0bcacbef6
Jamie McDonald <jamie@qwyck.net>
2026-05-03 11:54:24 +0100

    fix: fix logging paths relative to chezmoi source dir

--- home/.chezmoiscripts/run_before_00_init_logs.sh
+++ home/.chezmoiscripts/run_before_00_init_logs.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -eufo pipefail
 
-printf '' > "${CHEZMOI_SOURCE_DIR:-${HOME}/.local/share/chezmoi}/home/debug-log.txt"
+printf '' > "${CHEZMOI_SOURCE_DIR:-${HOME}/.local/share/chezmoi/home}/debug-log.txt"
 
-. "${CHEZMOI_SOURCE_DIR:-${HOME}/.local/share/chezmoi}/lib/log.sh"
+. "${CHEZMOI_SOURCE_DIR:-${HOME}/.local/share/chezmoi}/../lib/log.sh"
 
 log::start
 
